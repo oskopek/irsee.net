5e48fdf0b69f785ff363a3d8652d2a6c1fbc6ccb
Ondrej Skopek <skopekondrej@gmail.com>
2017-02-02 11:24:59 +0100

    [skip ci] out-commented run in docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 docker build -t registry.gitlab.com/oskopek/irsee.net .
-docker run -it registry.gitlab.com/oskopek/irsee.net
+#docker run -it registry.gitlab.com/oskopek/irsee.net
 
